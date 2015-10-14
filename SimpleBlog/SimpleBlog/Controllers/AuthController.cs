ccfd94582adae45dab8139a7778056b6338411d4
unknown <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-13 21:31:06 -0500

    Implemented form authentication and custom role provider

--- SimpleBlog/SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/SimpleBlog/Controllers/AuthController.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using SimpleBlog.ViewModels;
 
 namespace SimpleBlog.Controllers
 {
     public class AuthController : Controller
     {
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToRoute("home");
+        }
+
         public ActionResult Login()
         {
             return View(new AuthLogin
@@ -17,20 +24,19 @@ namespace SimpleBlog.Controllers
         }
 
         [HttpPost]
-        public ActionResult Login(AuthLogin form)
+        public ActionResult Login(AuthLogin form, string returnUrl)
         {
             if (!ModelState.IsValid)
             {
                 return View(form);
             }
 
-            if (form.Username != "a username")
-            {
-                ModelState.AddModelError("Username", "Username or password is not right");
-                return View(form);
-            }
+            FormsAuthentication.SetAuthCookie(form.Username, true);
 
-            return Content("The form is valid!");
+            if (!string.IsNullOrWhiteSpace(returnUrl))
+                return Redirect(returnUrl);
+
+            return RedirectToRoute("home");
         }
     }
 }
