d0c0a8d1c5f0fcb49494518ca5b923119483dbca
BarrettNashville <AG17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-14 22:16:00 -0500

    Displayed users from database and implemented Create User functionality (only first 25 mins of video)

--- SimpleBlog/SimpleBlog/Areas/Admin/ViewModels/Users.cs
+++ SimpleBlog/SimpleBlog/Areas/Admin/ViewModels/Users.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using NHibernate.Type;
 using SimpleBlog.Models;
 
 namespace SimpleBlog.Areas.Admin.ViewModels
@@ -8,5 +10,17 @@ namespace SimpleBlog.Areas.Admin.ViewModels
     {
          public IEnumerable<User> Users {get; set; }
 
+    }
+
+    public class UsersNew
+    {
+        [Required, MaxLength(128)]
+        public string Username { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required, MaxLength(256), DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
     }
 }
